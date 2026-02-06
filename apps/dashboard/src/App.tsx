f5995343bc54287ba6539f2318b5dfc5f1758613
Piyush <piyush.rp22@gmail.com>
2026-02-06 13:16:03 -0800

    Initialized dashboard

---
+++ apps/dashboard/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold text-start-jr-blue">Welcome Back</h1>
+      
+    </div>
+  )
+}
+
+export default App
